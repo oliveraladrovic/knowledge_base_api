ba825d6b09c78482b54e60679ef682e47ed942d1
Oliver Aladrovic <oliver.aladrovic@gmail.com>
2026-03-19 22:01:56 +0100

    Add sqlalchemy models file

---
+++ src/knowledge_base_api/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+
+    notes: Mapped[list["Note"]] = relationship(back_populates="user")
+
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    text: Mapped[str] = mapped_column(String)
+
+    user: Mapped[User] = relationship(back_populates="notes")
+    note_tags: Mapped[list["NoteTag"]] = relationship(back_populates="note")
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    tag: Mapped[str] = mapped_column(String)
+
+    note_tags: Mapped[list["NoteTag"]] = relationship(back_populates="tag")
+
+
+class NoteTag(Base):
+    __tablename__ = "note_tags"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    note_id: Mapped[int] = mapped_column(Integer, ForeignKey("notes.id"))
+    tag_id: Mapped[int] = mapped_column(Integer, ForeignKey("tags.id"))
+
+    note: Mapped[Note] = relationship(back_populates="note_tags")
+    tag: Mapped[Tag] = relationship(back_populates="note_tags")

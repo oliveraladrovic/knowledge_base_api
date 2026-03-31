5933e51a7ab5928c1b8d31acf1604b1880f14198
Oliver Aladrovic <oliver.aladrovic@gmail.com>
2026-03-31 18:12:59 +0200

    Add pydantic-settings config file and .env with database url

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
+# KNOWLEDGE BASE API PROJEKT
+from src.knowledge_base_api.config import settings
+from src.knowledge_base_api.models import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,14 +22,13 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from src.knowledge_base_api.models import Base
-
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 
 def run_migrations_offline() -> None:
--- src/knowledge_base_api/database.py
+++ src/knowledge_base_api/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/knowledge_base"
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(bind=engine)
 
 
